1fbc7a9a3292c06715f3ebbc1450e1d7da301b68
DaveOGQ <otigeorgedavid@gmail.com>
2024-08-14 18:39:32 -0600

    project scaffolding and dependecies complete

---
+++ db.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { Pool } from "pg";
+const pool = new Pool({
+  host: process.env.HOST,
+  port: process.env.PORT || 5432, // Default port is 5432 if not specified
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+module.exports = pool;
