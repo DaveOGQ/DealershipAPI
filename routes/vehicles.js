3cce0e0af61efe24bd860f7d09f5523df34c8620
DaveOGQ <otigeorgedavid@gmail.com>
2024-08-29 10:51:30 -0600

    Get all vehicles

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 //all routes from this file will be prefixed with the path "/users" in server js thus "/" here is actually "/users"
 
-router.get("/", (req, res) => {
-  res.send("Vehicle list");
+/* || HOME PATH ROUTE FOR GETTING ALL VEHICLES AND ADDING A NEW VEHICLE || */
+router.route("/").get(async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM vehicle");
+
+    result.rows.length > 0
+      ? res.status(200).json(result.rows)
+      : res.status(400).send("No vehicles exist.");
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send("An unexpected error occurred. Please try again later.");
+  }
 });
 
 module.exports = router;
